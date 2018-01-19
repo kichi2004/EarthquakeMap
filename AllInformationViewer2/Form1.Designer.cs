46ce2535c14a95c8ed8d9ad7c6d64dfb385b0489
Kichi2525yt <Kichi_2525yt@outlook.jp>
2018-01-19 10:30:14 +0900

    初期コミット

---
+++ AllInformationViewer2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace AllInformationViewer2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nowtime = new System.Windows.Forms.Label();
+            this.infoType = new System.Windows.Forms.Label();
+            this.kyoshinMonitor = new System.Windows.Forms.PictureBox();
+            this.mainPicbox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kyoshinMonitor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicbox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nowtime
+            // 
+            this.nowtime.Font = new System.Drawing.Font("Yu Gothic UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nowtime.Location = new System.Drawing.Point(2, -13);
+            this.nowtime.Name = "nowtime";
+            this.nowtime.Size = new System.Drawing.Size(218, 68);
+            this.nowtime.TabIndex = 0;
+            this.nowtime.Text = "07:00:00";
+            this.nowtime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoType
+            // 
+            this.infoType.BackColor = System.Drawing.Color.Transparent;
+            this.infoType.Font = new System.Drawing.Font("Yu Gothic UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.infoType.Location = new System.Drawing.Point(2, 41);
+            this.infoType.Name = "infoType";
+            this.infoType.Size = new System.Drawing.Size(218, 51);
+            this.infoType.TabIndex = 1;
+            this.infoType.Text = "各地の震度";
+            this.infoType.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // kyoshinMonitor
+            // 
+            this.kyoshinMonitor.Location = new System.Drawing.Point(0, 90);
+            this.kyoshinMonitor.Name = "kyoshinMonitor";
+            this.kyoshinMonitor.Size = new System.Drawing.Size(220, 250);
+            this.kyoshinMonitor.TabIndex = 2;
+            this.kyoshinMonitor.TabStop = false;
+            // 
+            // mainPicbox
+            // 
+            this.mainPicbox.Location = new System.Drawing.Point(219, 0);
+            this.mainPicbox.Name = "mainPicbox";
+            this.mainPicbox.Size = new System.Drawing.Size(523, 340);
+            this.mainPicbox.TabIndex = 3;
+            this.mainPicbox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 340);
+            this.Controls.Add(this.mainPicbox);
+            this.Controls.Add(this.kyoshinMonitor);
+            this.Controls.Add(this.infoType);
+            this.Controls.Add(this.nowtime);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kyoshinMonitor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicbox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nowtime;
+        private System.Windows.Forms.Label infoType;
+        private System.Windows.Forms.PictureBox kyoshinMonitor;
+        private System.Windows.Forms.PictureBox mainPicbox;
+    }
+}
+
---
+++ AllInformationViewer2/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+using Timer = System.Timers.Timer;
+
+namespace AllInformationViewer2
+{
+    public partial class Form1 : Form
+    {
+        private DateTime _now;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _ = Handle;
+            Initialize();   
+        }
+
+        private async void Initialize()
+        {
+            var timer = new Timer(100);
+            timer.Elapsed += this.TimerElapsed;
+            //TODO: 例外処理
+            await SetTime();
+            timer.Start();
+        }
+
+        private async void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime time;
+            if (_now.Minute % 10 == 0 &&
+                _now.Second == 0 && _now.Millisecond <= 100) {
+                await SetTime();
+            } else {
+                _now = _now.AddMilliseconds(100);
+            }
+            time = _now;
+            if (time.Millisecond > 100) return;
+
+            this.BeginInvoke( new Action(() =>
+            nowtime.Text = _now.ToString("HH:mm:ss")));
+            time = time.AddSeconds(-1);
+            //強震モニタ画像取得
+            string kmoniUrl = 
+                $"http://www.kmoni.bosai.go.jp/new/data/map_img/RealTimeImg/" +
+                $"jma_s/{time:yyyyMMdd}/{time:yyyyMMddHHmmss}.jma_s.gif";
+            var kmoniImage = await DownloadImageAsync(kmoniUrl);
+            //できれば予測震度とか載せたいけどとりあえず放置
+            kyoshinMonitor.Image = kmoniImage;
+
+
+        }
+
+        /// <summary>
+        /// 時刻を合わせます。
+        /// </summary>
+        private async Task SetTime()
+        {
+            var str = 
+                (await DownloadStringAsync("http://ntp-a1.nict.go.jp/cgi-bin/jst"))
+                .Split('\n')[3];
+            var time = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                .AddSeconds(double.Parse(str)).ToLocalTime();
+            _now = time;
+        }
+        
+        
+
+
+        /// <summary>
+        /// 指定されたURLから文字列をダウンロードします。
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>ダウンロードされた文字列</returns>
+        private async Task<string> DownloadStringAsync(string url)
+        {
+            if (url == null)
+                throw new ArgumentNullException(nameof(url));
+            using (var http = new HttpClient())
+                return await http.GetStringAsync(url).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// 指定されたURLから画像をダウンロードします。
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>ダウンロードされた画像</returns>
+        private async Task<Bitmap> DownloadImageAsync(string url)
+        {
+            if (url == null)
+                throw new ArgumentNullException(nameof(url));
+            using (var http = new HttpClient()) {
+                var stream = await http.GetStreamAsync(url).ConfigureAwait(false);
+                return new Bitmap(stream);
+            }
+        }
+
+    }
+}
