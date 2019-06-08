e65351372fbb63eea48e62646b5643236f918513
Takayuki Ueno <mail@kichi2004.jp>
2019-06-08 17:12:07 +0900

    コードフォーマット修正

--- EarthquakeMap/VersionChecker.cs
+++ EarthquakeMap/VersionChecker.cs
@@ -17,15 +17,15 @@ namespace EarthquakeMap
         {
             _webClient = new WebClient {Encoding = Encoding.UTF8};
         }
-        private WebClient _webClient;
-        private const string URL = "http://api.kichi2004.jp/eqmap/version.json";
+        private readonly WebClient _webClient;
+        private const string Url = "http://api.kichi2004.jp/eqmap/version.json";
 
         public void Check()
         {
             var asm = Assembly.GetExecutingAssembly();
             var ver = asm.GetName().Version;
 
-            var raw = _webClient.DownloadString(URL);
+            var raw = _webClient.DownloadString(Url);
             var json = DynamicJson.Parse(raw);
             string[] arr = json.oldversions;
 
